cd2c856ca3ae8c654231a096a387556371d06948
Simon Käser <skaeser@raptus.com>
2011-06-03 07:49:57 +0000

    initial working version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.0a1'
 
 setup(name='raptus.article.backlink',
       version=version,
-      description="Provides a component displaying a backlink using raptus.backlink",
+      description="Provides a component displaying a back link using raptus.backlink",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
