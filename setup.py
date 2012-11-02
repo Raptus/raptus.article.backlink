956f062f5657688c826983a2b563b61b4bedb9c1
Jonas Wagner <jwagner@raptus.com>
2012-11-02 02:46:11 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0a1'
 setup(name='raptus.article.backlink',
       version=version,
       description="Provides a component displaying a back link using raptus.backlink",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='raptus.article.backlink',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.article/raptus.article.backlink',
+      url='https://github.com/Raptus/raptus.article.backlink',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus', 'raptus.article'],
